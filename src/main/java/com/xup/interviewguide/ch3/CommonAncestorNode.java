5ea358e4adf07a97cce681e726d058aeef4eb0b3
panxu18 <xp545945@163.com>
2019-08-14 20:56:04 +0800

    update TraversalTree

--- src/main/java/com/xup/interviewguide/ch3/CommonAncestorNode.java
+++ src/main/java/com/xup/interviewguide/ch3/CommonAncestorNode.java
@@ -1,5 +1,17 @@
 package com.xup.interviewguide.ch3;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+
+import com.xup.interviewguide.ch2.TransferTree.TreeNode;
+import com.xup.interviewguide.ch3.PrintTree.NodeType;
+
 public class CommonAncestorNode {
 
     /*
@@ -7,7 +19,7 @@ public class CommonAncestorNode {
     * 就可以得到公共祖先。采用这种思路，需要保存所有节点
     * 到根节点的路径，每个节点的路径的长度为O(h)，同时在
     * 比较路径时需要花的时间为为O(h)。所以这个方法的时间
-    * 复杂度为O(n+m*h),空间复杂度为O(n*h)。
+    * 复杂度为O(n),空间复杂度为O(n*h)。
     *
     * 在上面的方法中，存在重复的比较，例如，一个节点的深度
     * 很深，也就是它到根节点的路径很长。现在要求它的左节点
@@ -23,6 +35,176 @@ public class CommonAncestorNode {
     * 节点时就知道两个节点分别位于当前子树的左右两边，所以公共
     * 祖先就是当前子树的根节点。
     *
-    * 根据前面的分析，在遍历的过程中
+    * 下面的实现代码中通过commonAncestor表示当前子树的根
+    * 节点，nextCommonAncestor表示下一棵子树的根节点。
+    * 
      */
+	public static TreeNode getCommonAncestor(TreeNode root, int k1, int k2) {
+		if (root == null)
+			return null;
+		Stack<TreeNode> stack = new Stack<>();
+		TreeNode commonAncestor = null;
+		TreeNode pre = null;
+		TreeNode cur = root;
+		while (!stack.isEmpty() || cur != null) {
+			if (cur != null) {
+				if (cur.val == k1 || cur.val == k2) {
+					if (commonAncestor == null)
+						commonAncestor = cur;
+					else
+						break;
+				}
+				stack.push(cur);
+				cur = cur.left;
+			} else if (stack.peek().right == null 
+					|| pre == stack.peek().right){
+				pre = stack.peek();
+				if (stack.pop() == commonAncestor)
+					commonAncestor = stack.peek();
+			} else 
+				cur = stack.peek().right;
+		}
+		return commonAncestor;
+	}
+	
+	public static int getCommonAncestor() {
+		Scanner input = new Scanner(System.in);
+		int nums = input.nextInt();
+		input.nextInt();
+		int[][] list = new int[nums][3];
+		for (int i = 0; i < nums; i++)
+			for (int j = 0; j < 3; j++)
+				list[i][j] = input.nextInt();
+		
+		int k1 = input.nextInt();
+		int k2 = input.nextInt();
+		int[] stack = new int[nums];
+		int top = -1;
+		int commonAncestor = -1;
+		int nextAncestor = -1;
+		input.nextLine();
+		for (int[] l : list) {
+			if (l[0] == k1 || l[0] == k2) {
+				if (commonAncestor == -1) {
+					commonAncestor = l[0];
+					nextAncestor = top == -1 ? -1 : stack[top];
+				}
+				else
+					break;
+			}
+			stack[++top] = l[0];
+			if (l[1] == 0 && stack[top--] == nextAncestor) {
+				commonAncestor = nextAncestor;
+				nextAncestor = top == -1 ? -1 : stack[top];
+			}
+			if (l[2] == 0 && stack[top--] == nextAncestor)  {
+				commonAncestor = nextAncestor;
+				nextAncestor = top == -1 ? -1 : stack[top];
+			}
+		}
+		
+		return commonAncestor;
+	}
+	
+	/*
+	 * 查找指定节点当前所在子树的根节点
+	 */
+	private static int getAncestor(HashMap<Integer, Integer> ancestorMap, int key) {
+		if (!ancestorMap.containsKey(key))
+			return 0;
+		int result = key;
+		// 查找当前子树的根节点
+		while (ancestorMap.get(result) != result) {
+			result = ancestorMap.get(result);
+		}
+		int temp;
+		// 压缩路径
+		while (key != (temp = ancestorMap.get(key))) {
+			ancestorMap.put(key, result);
+			key = temp;
+		}
+		return result;
+		
+	}
+	/*
+	 * 在m个查询条件同时查询时，需要在遍历时需要知道m对节点分别在
+	 * 哪一棵子树，所以需要保存m个当前子树的根节点以及下一棵子树的
+	 * 根节点。
+	 */
+	public static ArrayList<Integer> getCommonAncestorWithMultQuery(TreeNode root, ArrayList<ArrayList<Integer>> query) {
+		if (root == null)
+			return null;
+		
+		HashMap<Integer, ArrayList<Integer>> queryMap = new HashMap<>();
+		for (ArrayList<Integer> l : query) {
+			if (queryMap.get(l.get(0)) == null)
+				queryMap.put(l.get(0), new ArrayList<>());
+			queryMap.get(l.get(0)).add(l.get(1));
+			if (queryMap.get(l.get(1)) == null)
+				queryMap.put(l.get(1), new ArrayList<>());
+			queryMap.get(l.get(1)).add(l.get(0));
+		}
+		HashMap<Integer, Integer> ancestorMap = new HashMap<>();
+		HashMap<Integer, HashMap<Integer, Integer>> answer = new HashMap<>();
+		Stack<TreeNode> stack = new Stack<>();
+		TreeNode pre = null;
+		TreeNode cur = root;
+		while (!stack.isEmpty() || cur != null) {
+			if (cur != null) {
+				// 设置初始祖先节点为自身
+				ancestorMap.put(cur.val, cur.val);
+				/*
+				 *  如果查询条件中前一个节点的祖先节点已经求出来了，
+				 *  那么两个节点的共同节点为前一个节点的祖先
+				 */
+				if (queryMap.get(cur.val) != null) {
+					if (cur.val == 38)
+						System.out.println("test");
+					for (Integer v : queryMap.get(cur.val)) {
+						int ancestor = getAncestor(ancestorMap, v);
+						if (ancestor != 0
+								&& (answer.get(cur.val) != null
+								|| answer.put(cur.val, new HashMap<>()) == null))
+							answer.get(cur.val).put(v, ancestor);
+					}
+				}
+				stack.push(cur);
+				cur = cur.left;
+			} else if (stack.peek().right == null 
+					|| pre == stack.peek().right){
+				pre = stack.pop();
+				if (!stack.isEmpty())
+					ancestorMap.put(pre.val, stack.peek().val);
+			} else
+				cur = stack.peek().right;
+		}
+		// 整理结果
+		ArrayList<Integer> result = new ArrayList<>(query.size());
+		for (ArrayList<Integer> l : query) {
+			Integer r = null;
+			if (answer.containsKey(l.get(0))
+					&& (r = answer.get(l.get(0)).get(l.get(1))) != null) {
+				result.add(r);
+			} else 
+				result.add(answer.get(l.get(1)).get(l.get(0)));
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		TreeNode root = TreeNode.buildTree(input);
+		int nums = input.nextInt();
+		ArrayList<ArrayList<Integer>> query = new ArrayList<>();
+		for (int i = 0; i < nums; i++) {
+			ArrayList<Integer> l = new ArrayList<>(2);
+			l.add(input.nextInt());
+			l.add(input.nextInt());
+			query.add(l);
+		}
+		new PrintTree().print(root, 0, NodeType.ROOT);
+		getCommonAncestorWithMultQuery(root, query).stream()
+		.forEach(System.out::println);;
+	}
+	
 }
--- src/main/java/com/xup/interviewguide/ch3/TraversalTree.java
+++ src/main/java/com/xup/interviewguide/ch3/TraversalTree.java
@@ -1,11 +1,12 @@
 package com.xup.interviewguide.ch3;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 import com.xup.interviewguide.ch2.TransferTree.TreeNode;
 
 public class TraversalTree {
-	
+
 	/*
 	 * 递归先序遍历
 	 */
@@ -16,7 +17,7 @@ public class TraversalTree {
 		preOrderRecur(root.left);
 		preOrderRecur(root.right);
 	}
-	
+
 	/*
 	 * 递归中序遍历
 	 */
@@ -27,7 +28,7 @@ public class TraversalTree {
 		System.out.println(root.val);
 		preOrderRecur(root.right);
 	}
-	
+
 	/*
 	 * 递归后续遍历
 	 */
@@ -38,32 +39,36 @@ public class TraversalTree {
 		preOrderRecur(root.right);
 		System.out.println(root.val);
 	}
-	
+
 	/*
 	 * 非递归先序遍历
 	 */
-	public void preOrderUnRecur(TreeNode root) {
+	public static void preOrderUnRecur(TreeNode root) {
 		if (root == null)
 			return;
+		StringBuffer sb = new StringBuffer();
 		Stack<TreeNode> stack = new Stack<>();
 		TreeNode p = null;
+		stack.push(root);
 		while (!stack.isEmpty()) {
 			p = stack.pop();
 			if (p != null) {
-				System.out.println(p.val);
+				sb.append(" " + p.val);
 				if (p.right != null)
 					stack.push(p.right);
 				if (p.left != null)
 					stack.push(p.left);
 			}
 		}
+		System.out.println(sb.deleteCharAt(0));
 	}
-	
+
 	/*
 	 * 非递归中序遍历
 	 */
-	public void inOrderUnRecur(TreeNode root) {
+	public static void inOrderUnRecur(TreeNode root) {
 		Stack<TreeNode> stack = new Stack<>();
+		StringBuffer sb = new StringBuffer();
 		while (!stack.isEmpty() || root != null) {
 			while (root != null) {
 				stack.push(root);
@@ -71,49 +76,47 @@ public class TraversalTree {
 			}
 			if (!stack.isEmpty()) {
 				root = stack.pop();
-				System.out.println(root.val);
+				sb.append(" " + root.val);
 				root = root.right;
 			}
 		}
+		System.out.println(sb.deleteCharAt(0));
 	}
-	
+
 	/*
 	 * 非递归后续遍历
 	 * 从根节点开始执行以下步骤
-	 * 1、当前节点左子树不为空且前驱节点不是当前节点的子节点，遍历左子树
-	 * 2、当前节点左子树为空时，右子树不为空且前驱节点不是当前右节点，则遍历右子树
-	 * 3、当前节点右子树为空或者前驱节点是当前节点的右节点，访问当前节点，返回到父节点
+	 * 1、当前节点不为空，遍历左子树
+	 * 2、当前节点为空，栈顶节点没有右子树，或者右子树已经遍历，则访问当前节点
+	 * 3、否则遍历右子树
 	 */
-	public void postOrderUnRecur(TreeNode root) {
-		Stack<TreeNode> stack = new Stack<>();
-		if (root == null) 
+	public static void postOrderUnRecur(TreeNode root) {
+		if (root == null)
 			return;
+		Stack<TreeNode> stack = new Stack<>();
+		StringBuffer sb = new StringBuffer();
 		TreeNode pre = null;
-		stack.push(root);
-		while (!stack.isEmpty()) {
-			root = stack.peek();
-			if (root.left != null && pre != root.left && pre != root.right) 
-				stack.push(root.left);
-			else if (root.right != null && pre != root.right)
-				stack.push(root.right);
-			else {
+		TreeNode cur = root;
+		while (!stack.isEmpty() || cur != null) {
+			if (cur != null) {
+				stack.push(cur);
+				cur = cur.left;
+			} else if (stack.peek().right == null 
+					|| pre == stack.peek().right){
+				sb.append(" " + stack.peek().val);
 				pre = stack.pop();
-				System.out.println(pre);
-			}
+			} else 
+				cur = stack.peek().right;
 		}
-//		while (!stack.isEmpty()) {
-//			root = stack.peek();
-//			if ((root.left == null && root.right == null)
-//					|| (pre == root.left && root.right == null)
-//					|| pre == root.right) {
-//				pre = stack.pop();
-//				System.out.println(pre);
-//			} else if (root.left != null && pre != root.left) 
-//				stack.push(root.left);
-//			else
-//				stack.push(root.right);
-//				
-//		}
+		System.out.println(sb.deleteCharAt(0));
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		TreeNode root = TreeNode.buildTree(input);
+		preOrderUnRecur(root);
+		inOrderUnRecur(root);
+		postOrderUnRecur(root);
 	}
 
 }
